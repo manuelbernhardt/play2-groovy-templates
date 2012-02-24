1586f0a3d0c8c2ad8ae2bde66898d18fb1f363b5
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-02-24 12:50:08 +0100

    Bumping 1.0-SNAPSHOT

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -3,15 +3,15 @@ import Keys._
 
 object PluginBuild extends Build {
 
-  val buildVersion = "0.1-SNAPSHOT"
+  val buildVersion = "1.0-SNAPSHOT"
 
-  val delvingReleases = "Delving Releases Repository" at "http://development.delving.org:8081/nexus/content/groups/public"
+  val delvingReleases = "Delving Releases Repository" at "http://development.delving.org:8081/nexus/content/repositories/releases"
   val delvingSnapshots = "Delving Snapshot Repository" at "http://development.delving.org:8081/nexus/content/repositories/snapshots"
   val delvingRepository = if(buildVersion.endsWith("SNAPSHOT")) delvingSnapshots else delvingReleases
 
   val dependencies = Seq(
     "play"                  %% "play"                         % "2.0-RC3-SNAPSHOT",
-    "eu.delving"            %  "groovy-templates-engine"      % "0.1-SNAPSHOT",
+    "eu.delving"            %  "groovy-templates-engine"      % "0.5",
     "commons-io"            %  "commons-io"                   % "2.0"
   )
 
