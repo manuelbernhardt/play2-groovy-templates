c492114896275e34cc56c2c07efe8f43951aebab
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-07-27 16:27:26 +0200

    1.5

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object PluginBuild extends Build {
 
-  val buildVersion = "1.4"
+  val buildVersion = "1.5"
 
   val delvingReleases = "Delving Releases Repository" at "http://development.delving.org:8081/nexus/content/repositories/releases"
   val delvingSnapshots = "Delving Snapshot Repository" at "http://development.delving.org:8081/nexus/content/repositories/snapshots"
